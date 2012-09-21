a56ca644ea015b057f27fd41071942ead1b3bd77
Falcon Dai <me@falcondai.com>
2012-09-21 13:35:28 -0500

    implemented d3js local cache, better color scale, slider+datepicker, hover-box.

--- mention_map/test_server.py
+++ mention_map/test_server.py
@@ -6,13 +6,18 @@ import urllib2 as ul
 app = flask.Flask(__name__)
 
 @app.route('/')
-def hello():
-	return 'This is a simple dev server.'
+def index():
+	return 'This is a simple dev server. <br />site map:<br />'+reduce(lambda x,y: str(x)+'<br />'+str(y), app.url_map._rules)
 	
 @app.route('/count.json')
 def count():
 	time = flask.request.args.get('time', '0')
 	d = ul.urlopen('http://twitwi.mit.edu:5000/count?time='+time)
+	return d.read()
+
+@app.route('/static/dat/times.json')
+def times():
+	d = ul.urlopen('http://twitwi.mit.edu:5000/static/dat/times.json')
 	return d.read()
 	
 @app.route('/map')
